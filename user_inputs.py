6d50ff66c93f3a64a8b1a05a1965a50cd74ad5fb
dan <@danray472>
2024-04-24 21:08:54 +0300

    assignment 1 python

---
+++ user_inputs.py
@@ -0,0 +1,4 @@
+user_name = input("Enter you name: ")
+user_age = input("Enter you age: ")
+user_location = input("Where do you leave: ")
+print("Your name is" + " " + user_name + " " + "and you are" + " " + user_age + "yrs old. You leave in" + " " + user_location)
